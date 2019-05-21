4926a1c7669e3dc32dae29afbc759152f10f3815
Chengsi <gaochengsi@gmail.com>
2019-05-20 17:14:36 -0700

    adding first problem

---
+++ combination.js
@@ -0,0 +1,20 @@
+/**
+ * design an algorithm to return the number of 
+ * all permutations of a string;
+ * Assume all characters are unique.
+ * eg: 'a' => {'a'} //1
+ *     'ab' => {'ab', 'ba'} //2
+ *     'abc' 
+ * => {'cab', 'acb', 'abc', 'cba', 'bca', 'bac'} //6
+ */
+
+function combination(str) {
+    if (str.length === 0) return 0;
+    if (str.length === 1) return 1;
+
+    
+    if (str.length === 2) 
+  
+}
+
+module.exports = combination
